57282f92053db5aa4c9a5c80d54bd23530db39e6
freemanro <freeman.ro@gmail.com>
2022-07-05 13:54:20 +0430

    add part2

--- src/Hw9Application.java
+++ src/Hw9Application.java
@@ -1,3 +1,5 @@
+import org.javatuples.Pair;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,6 +9,7 @@ public class Hw9Application {
     public static void main(String[] args) {
         String[] strings = {"i", "was", "@", "aut", "90", "", ""};
         part1(strings);
+        part2(strings);
     }
 
     private static void part1(String[] strings) {
@@ -21,3 +24,26 @@ public class Hw9Application {
                 (k, v) -> System.out.println("Strings with length " + k + ": " + v));
 
     }
+
+    private static void part2(String[] strings) {
+        Map<Integer, Pair<Integer, List<String>>> classesOfLength =
+                Stream.of(strings).collect(
+                        Collectors.groupingBy(
+                                str -> str.length(),
+                                Collectors.collectingAndThen(Collectors.toList(), list -> {
+                                    long count = list.size();
+                                    return new Pair(list, count);
+                                })
+                        )
+                );
+
+        classesOfLength.forEach(
+                (k, v) ->
+                        System.out.println(
+                                "class of length " + k +
+                                        ": (count = " + v.getValue1() + ") " +
+                                        v.getValue0()
+                        )
+        );
+    }
+}
